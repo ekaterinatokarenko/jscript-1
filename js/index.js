4122bc5afdbabc9202fbc45bd493f1a29166b72a
Kate <ekaterinatokarenko777@gmail.com>
2017-12-21 17:47:47 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/*alert ('Kate, u are so beautiful!');
+let years = prompt('Сколько вам лет?', '');
+let question = confirm('It is ok!');*/
+
+let name = prompt('What is your name?', '');
+let userName = alert(name);
