c38f1d41cf5ab9544ca40b1430d358da1c9c7622
Kate <ekaterinatokarenko777@gmail.com>
2017-12-22 21:00:23 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -2,5 +2,61 @@
 let years = prompt('Сколько вам лет?', '');
 let question = confirm('It is ok!');*/
 
-let name = prompt('What is your name?', '');
-let userName = alert(name);
+/*let name = prompt('What is your name?', '');
+let userName = alert(name);*/
+
+/*let question = prompt('What is the main name of JavaScript?', '');
+if (question == 'ECMAScript') {
+    alert ('You are true');
+}
+else {
+    alert('ECMAScript!');
+}*/
+
+/*let number = prompt('Enter your number','');
+if (number == 1){
+    alert ('1');
+}
+else if (number == 0){
+    alert ('0');
+}
+else {
+    alert ('-1');
+}*/
+
+let login = prompt('Enter your login', '');
+if (login == 'Admin'){
+    let password = prompt('Enter your password','');
+    if (password == 1234){
+        alert('You are welcome');
+    }
+    else if (password == null){
+        alert('Enter canceled');
+    }
+    else {
+        alert('Incorrect password');
+    }
+}
+else if (login == null){
+    alert('Enter canceled');
+}
+else{
+    alert('I do not know you');
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
